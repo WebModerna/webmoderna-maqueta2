0f265b3c17ca07b31df37d19e97233b44ee41432
WebModerna <info@webmoderna.com.ar>
2016-09-24 16:15:50 -0300

    Segundo comit

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt)
+{
+	grunt.initConfig({
+
+		// Unificar y minificar scripts
+		uglify: {
+
+			// Scripst generales
+			mi_prueba: {
+				files: {
+					'js/scripts.min.js': [
+						'js/jquery.js',
+						'js/owl.carousel.js',
+						'js/jquery.placeholder.js',
+						'js/jquery.swipebox.js',
+						// 'js/placeholder.js',
+						'js/scripts.js'
+					]
+				}
+			},
+
+			// Scripts del multidatepicker
+			mi_multidatepicker: {
+				/*files: {
+					'js/multidatepicker.min.js': [
+						'js/jquery-1.11.1.js',
+						'js/jquery-ui-1.11.1.js',
+						'js/jquery-ui.multidatespicker.js'
+					]
+				}*/
+			}
+		},
+
+		// Monitorear cambios
+		watch: {
+			// Los Scripts
+			scripts: {
+				files: ['js/*.js'],
+				tasks: ['uglify'],
+				options: {
+					spawn: false
+				}
+			}
+		}
+	});
+
+	// grunt.loadNpmTasks('grunt-postcss');
+	// grunt.registerTask('css', ['stylus', 'postcss']);
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	// grunt.loadNpmTasks('grunt-contrib-imagemin');
+};
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # webmoderna-maqueta2
 Plantilla nueva de WebModerna
-tiene muchas mejoras
